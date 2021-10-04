d70614b401ee30ad7e29017df1a026384ac0f4fb
martin <martinerasmus1999@gmail.com>
2021-10-04 20:50:11 +0200

    upload button renamed

--- PRG-282-Project-Group-3/PRG282_Project_Group3/CaptureFrm.Designer.cs
+++ PRG-282-Project-Group-3/PRG282_Project_Group3/CaptureFrm.Designer.cs
@@ -230,8 +230,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 11;
-            this.button1.Text = "button1";
+            this.button1.Text = "Upload";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dateTimePicker1
             // 
--- PRG-282-Project-Group-3/PRG282_Project_Group3/CaptureFrm.cs
+++ PRG-282-Project-Group-3/PRG282_Project_Group3/CaptureFrm.cs
@@ -21,5 +21,10 @@ namespace PRG282_Project_Group3
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
